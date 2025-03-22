e633044988bc447ab966d54300f3636bd394fa51
Jhoan David Sinisterra <sinisterravalenciajhoandavid@gmail.com>
2025-03-22 17:27:16 -0500

    Modificación de main.py haciendo una explicación de comandos git

--- main.py
+++ main.py
@@ -9,6 +9,11 @@ print("3. git config --global user.email '' añadir el email del usuario")
 
 
 print("-COMANDOS PRINCIPALES TRABAJADOS CON GIT-")
+
 print("git status: Funciona para ver el estado de los archivos de mi repositorio local")
+
 print("git add 'nombre_archivo' o git add . (punto añade todos los archivos)")
 
+print("git commit -m '': commit(el trabajo o cambios que realizas) es para hacerle preparar los archivos que se van a enviar al repositorio remoto y -m 'Aqui va el mensaje describiendo los cambios que realizaste'")
+
+print("git push origin main: Se envian los commits que tienes en tu repositorio local a tu repositorio remoto(github) al cual estas vinculado")
