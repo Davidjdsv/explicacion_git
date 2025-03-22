45dcecf02366f2793983208d49adf66f1285745a
Jhoan David Sinisterra <sinisterravalenciajhoandavid@gmail.com>
2025-03-22 16:52:45 -0500

    Primer envio del proyecto

---
+++ main.py
@@ -0,0 +1,9 @@
+print("Hola Karol")
+
+print("Comandos para git")
+print("1: git init: Para incializar el repositorio local")
+
+print("2: Hacer configuraciones inciales: git config --global user.name '' (Para asignar el nombre del usuario del repositorio local)")
+
+print("3. git config --global user.email '' añadir el email del usuario")
+print()
