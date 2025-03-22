8257c9117f31a347019177224b714bc1a0a67822
Jhoan David Sinisterra <sinisterravalenciajhoandavid@gmail.com>
2025-03-22 17:22:18 -0500

    Actualización en main.py

--- main.py
+++ main.py
@@ -6,4 +6,9 @@ print("1: git init: Para incializar el repositorio local")
 print("2: Hacer configuraciones inciales: git config --global user.name '' (Para asignar el nombre del usuario del repositorio local)")
 
 print("3. git config --global user.email '' añadir el email del usuario")
-print()
+
+
+print("-COMANDOS PRINCIPALES TRABAJADOS CON GIT-")
+print("git status: Funciona para ver el estado de los archivos de mi repositorio local")
+print("git add 'nombre_archivo' o git add . (punto añade todos los archivos)")
+
